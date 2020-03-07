4359b8b22feb8f8596252969de42deed3ed70dfc
EdgarsPo <48221021+EdgarsPo@users.noreply.github.com>
2020-03-07 14:21:27 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+
+
+<html>
+<head>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+	
+<h2>Registration form</h2>
+<h4>Username</h4>
+
+<form method="post" action="index.php">
+<input type="text" placeholder="Username" name="username" required>
+<div>
+<button type="submit" name="register" class="btn">Register</button>
+</div>
+</form>
+
+<h3>Already register?</h3>
+<label>Username</label>
+
+<form method="post" action="index.php">
+<input type="text" placeholder="Username" name="username" required>
+<div>
+<button type="submit" name="login" class="btn">Login</button>
+</div>
+</form>
+
+</body>
+</html>
